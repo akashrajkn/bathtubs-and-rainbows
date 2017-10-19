6ec38a7a26fa746b224340a918a8bf6abbe7d847
akashrajkn <akashrajkn@gmail.com>
2017-10-19 22:26:24 +0200

    Simulator - initial

---
+++ src/main.py
@@ -0,0 +1,39 @@
+import copy
+
+import defaults
+
+
+def proportional(source, dest, sign):
+    pass
+
+
+def influence(source, dest, sign):
+    pass
+
+
+def current_state():
+    pass
+
+
+def next_state():
+    pass
+
+
+def previous_state():
+    pass
+
+
+def exogenous():
+    pass
+
+
+def simulate():
+    # Get initial state
+    initial_state = defaults['initial_state'].copy()
+
+
+
+
+
+if __name__ == '__main__':
+    simulate()
